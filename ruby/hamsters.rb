35101f0c74d65000f6a0fa98192fa1f92e24d3e5
splashthatcat <carolyn.freney@gmail.com>
2017-02-15 17:10:54 -0500

    Add files via upload

---
+++ ruby/hamsters.rb
@@ -0,0 +1,21 @@
+##Incoming Hamster Questionaire
+
+puts "What's the hamster's name?"
+hamster_name = gets.chomp
+print "Name:" hamster_name
+
+puts "What's the little critter's volume level?"
+volume_level = gets.to_i
+print "Volume Level:" volume_level
+
+puts "What's their fur color?"
+fur_color = gets.chomp
+print "Fur Color:" fur_color
+
+puts "Is this hamster a good candidate for adoption?"
+candidate_for_adoption = gets.chomp
+print "Candidate For Adoption:" candidate_for_adoption
+
+puts "What's their estimated age?"
+estimated_age = gets.to_i
+print "Age:" estimated_age
